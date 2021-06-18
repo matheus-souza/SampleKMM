05e6a36cdc18ff00c6a48cb9d59ed5d47b4f6558
Matheus H. de Souza <mh.matheussouza@gmail.com>
2021-06-18 15:28:42 -0300

    feat: implementado refresh do quadrinho

--- androidApp/src/main/java/com/example/samplekmm/androidApp/ui/main/MainActivity.kt
+++ androidApp/src/main/java/com/example/samplekmm/androidApp/ui/main/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.samplekmm.androidApp.ui.main
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.samplekmm.androidApp.R
+import com.example.samplekmm.androidApp.databinding.ActivityMainBinding
 import com.example.samplekmm.androidApp.remote.State
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -13,25 +13,53 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupObservers()
+        setupListeners()
+
+        viewModel.fetchComic()
+    }
+
+    private fun setupObservers() {
         viewModel.comic.observe(this) {
             when (it) {
                 is State.Loading -> {
-                    findViewById<TextView>(R.id.titleLabel).text = "Loading"
+                    disableButton()
+                    binding.titleLabel.text = "Loading"
                 }
                 is State.Success -> {
-                    findViewById<TextView>(R.id.titleLabel).text = it.result.title
+                    enableButton()
+                    binding.titleLabel.text = it.result.title
                     Glide.with(this)
                         .load(it.result.imageUrl)
                         .into(findViewById(R.id.image))
                 }
                 is State.Error -> {
-                    findViewById<TextView>(R.id.titleLabel).text = "Error"
+                    enableButton()
+                    binding.titleLabel.text = "Error"
                 }
             }
         }
+    }
 
-        viewModel.fetchComic()
+    private fun setupListeners() {
+        binding.btnRefresh.setOnClickListener {
+            viewModel.fetchComic()
+        }
+    }
+
+    private fun enableButton() {
+        binding.btnRefresh.isEnabled = true
+    }
+
+    private fun disableButton() {
+        binding.btnRefresh.isEnabled = false
     }
 }
